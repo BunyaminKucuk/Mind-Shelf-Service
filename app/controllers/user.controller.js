63493e230eaa29b009c9c520110966024987b473
Bünyamin <bunyaminkcuk@gmail.com>
2020-10-27 16:52:35 +0300

    feat : added user-controller and routes

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,77 @@
+const db = require("../models"); const User = db.user;
+var bcrypt = require("bcryptjs");
+
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+};
+exports.userBoard = (req, res) => {
+    res.status(200).send("User Content.");
+};
+
+
+exports.updateUserStatus = (req, res) => {
+    User.update(
+        { UserStatus: req.query.user_status },
+        { returning: true, where: { UserID: req.query.user_id } }
+    )
+        .then((result) => {
+            res.status(200)
+                .send({
+                    message: 'UserStatus update success!',
+                    result: result,
+                });
+        })
+        .catch((err) => {
+            res.status(500)
+                .send({
+                    message: err
+                });
+        });
+}
+
+exports.profileEdit = (req, res) => {
+    User.update(
+        {
+            UserName: req.body.UserName,
+            UserFirstName: req.body.UserFirstName,
+            UserLastName: req.body.UserLastName,
+            UserMail: req.body.UserMail,
+            UserType: req.body.UserType, // admin
+            UserStatus: req.body.UserStatus, // aktif
+            UserPassword: bcrypt.hashSync(req.body.UserPassword, 12),
+        },
+        { returning: true, where: { UserID: req.query.user_id } }
+    )
+        .then((result) => {
+            res.status(200)
+                .send({
+                    message: 'UserProfile update success!',
+                    result: result,
+                });
+        })
+        .catch((err) => {
+            res.status(500)
+                .send({
+                    message: err
+                });
+        });
+}
+
+exports.userDelete = (req, res) => {
+    User.destroy({
+        where: {
+            UserID: req.query.user_id
+        }
+    })
+        .then(function (deletedRecord) {
+            if (deletedRecord === 1) {
+                res.status(200).json({ message: "Deleted successfully" });
+            }
+            else {
+                res.status(404).json({ message: "record not found" })
+            }
+        })
+        .catch(function (error) {
+            res.status(500).json(error);
+        });
+}
---
+++ app/routes/user.routes.js
@@ -0,0 +1,30 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+
+module.exports = function (app) {
+    
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    app.post(
+        "/api/admin/update-status",
+        [authJwt.verifyToken],
+        controller.updateUserStatus,
+    )
+    app.post(
+        "/api/user/profile-edit",
+        [authJwt.verifyToken],
+        controller.profileEdit,
+    )
+    app.post(
+        "/api/admin/user-delete",
+        [authJwt.verifyToken],
+        controller.userDelete,
+    )
+};
