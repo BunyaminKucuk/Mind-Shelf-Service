008547f68702b9e5b5e58a799c9e46995ebc82b8
BunyaminKucuk <BunyaminKucuk@github.com>
2020-11-22 00:35:33 +0300

    feat:getbyıd

--- app/routes/comment.routes.js
+++ app/routes/comment.routes.js
@@ -37,4 +37,9 @@ module.exports = function (app) {
         //[authJwt.verifyToken],
         controller.getUserAllComments,
     );
+    app.get(
+        "/api/admin/comment/getbyid",
+        //[authJwt.verifyToken],
+        controller.getByIDComment,
+    );
 }
